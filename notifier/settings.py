8726db4f1f190079094dc53ea8c2b67a046818bb
Konstantin Motuskov <konstantin.motuskov@wilo.com>
2022-12-05 17:39:42 +0300

    Creates the start up documentation

--- notifier/settings.py
+++ notifier/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vnr!!#h$t1eo_wh^)bhur@ho)1_m0)&mg-_vwuv@3r3a2@(n+-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
